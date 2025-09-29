65c32dd823ca9114ce3244b48c7075ecb69d6ec6
Jesper <jesper.elovsson92@gmail.com>
2025-09-29 14:31:28 +0200

    Update Interval

--- src/script/loadSimpleView.js
+++ src/script/loadSimpleView.js
@@ -1,8 +1,9 @@
 import { run as runNetwork } from './basicNetworkInfo.js';
+import { run as runRestart } from './compRestart.js';
 //import { run as runSomething } from './modules/something.js';
 
-window.addEventListener('DOMContentLoaded', async () => {
-    //runSomething();             // synchronous
+window.addEventListener('DOMContentLoaded', async () => {    
     runNetwork();      // asynchronous
-    console.log('All done');
+    await runRestart();      // synchronous
+    console.log('All done, Captain!');
 });
--- src/script/preload.js
+++ src/script/preload.js
@@ -1,8 +1,6 @@
 const os = require('os');
 const { contextBridge} = require('electron');
 
-const uptimeInSeconds = os.uptime();
-
 contextBridge.exposeInMainWorld('systemInfo', {	
-	getStartTime: () => uptimeInSeconds
+	getStartTime: () => os.uptime()
 });
