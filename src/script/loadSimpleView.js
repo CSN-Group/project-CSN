9a59c8e8321ca341c040bf52703de4d5ebb05a14
smret <smreteab1@gmail.com>
2025-09-30 10:58:26 +0200

    Good to go

--- src/script/loadSimpleView.js
+++ src/script/loadSimpleView.js
@@ -1,9 +1,11 @@
 import { run as runNetwork } from './basicNetworkInfo.js';
 import { run as runRestart } from './compRestart.js';
+import { run as runPcName } from './pcNameInfo.js';
 //import { run as runSomething } from './modules/something.js';
 
 window.addEventListener('DOMContentLoaded', async () => {    
     runNetwork();      // asynchronous
     await runRestart();      // synchronous
+    runPcName();
     console.log('All done, Captain!');
 });
