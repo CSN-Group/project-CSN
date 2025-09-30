080b9e96b3840a16816639fa29ee74a9accb39a4
Jesper <jesper.elovsson92@gmail.com>
2025-09-30 11:14:24 +0200

    Simple CSS and structure change

--- src/script/main.js
+++ src/script/main.js
@@ -4,7 +4,7 @@ const path = require('path');
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 600,    
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
@@ -12,7 +12,7 @@ function createWindow() {
       sandbox: false,
     },
   });
-
+  win.setMenuBarVisibility(false);
   win.loadFile('src/simpleView.html');  
 }
 
