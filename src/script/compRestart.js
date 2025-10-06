d589e21cad93679c5f0c6cde77746219cdb649fd
unknown <nial2400@student.miun.se>
2025-10-06 15:13:06 +0200

    Megafix

--- src/script/compRestart.js
+++ src/script/compRestart.js
@@ -5,17 +5,15 @@ function formatTime(seconds) {
         return hrs//`${hrs}h ${mins}m ${secs}s`;
 }
 
-export async function run() {            
+async function runCompRestart() {
     const restartContainer = document.getElementById("restartInfo");
-    
-        
+
     async function updateUptime() {
         const runTimeHrs = formatTime(window.systemInfo.getStartTime());
-        window.systemInfo.setGlobal('compOnTimeHours', runTimeHrs);   
+        setGlobal("compOnTimeHours", runTimeHrs);
         restartContainer.innerText = `Computer been running for ${runTimeHrs} hours`;
     }
 
     await updateUptime();
     setInterval(updateUptime, 10000); // Completely unnecessary to update this? Only when starting the app?
 }
-
