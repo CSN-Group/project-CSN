eb4cd4935d43e5c05dc400f1b605a6dc2fa8cf3f
Jesper <jesper.elovsson92@gmail.com>
2025-10-06 14:00:30 +0200

    Back to square one

--- src/database/dbManager.js
+++ src/database/dbManager.js
@@ -4,19 +4,33 @@ const path = require('path');
 const dbPath = path.join(__dirname, 'database.db');
 const db = new sqlite(dbPath);
 
-exports.getReadings = function() { //Get all readings from the database, as an array of objects.
+function getReadings() { //Get all readings from the database, as an array of objects.
     const sql = 'SELECT * FROM readings';
     let stmt = db.prepare(sql);
     let res = stmt.all();
     return res;
 }
 
-//Convert
+//Create Unix Timestamp
 
-exports.addReading = function(upSpeed, downSpeed, wifiStr, ping, connectType) { //Add a new reading to the database.
+function addReading(upSpeed, downSpeed, wifiStr, ping, connectType) { //Add a new reading to the database.
     const unixStamp = new Date().getTime();
     const sql = `INSERT INTO readings (timeStamp, upSpeed, downSpeed, wifiStr, ping, connectType)
     VALUES (?, ?, ?, ?, ?, ?)`;
     const stmt = db.prepare(sql);
     stmt.run(unixStamp, upSpeed, downSpeed, wifiStr, ping, connectType);    
 }
+
+function deleteOldReadings(cutoffTime) { //Delete readings older than cutoffTime. Needed?
+    const sql = 'DELETE FROM readings WHERE timeStamp < ?';
+    const stmt = db.prepare(sql);
+    stmt.run(cutoffTime);
+}
+
+function deleteAllReadings() { //Delete all readings from the database.
+    const sql = 'DELETE FROM readings';
+    const stmt = db.prepare(sql);
+    stmt.run();
+}
+
+module.exports = { getReadings, addReading, deleteOldReadings, deleteAllReadings };
