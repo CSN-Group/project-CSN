0fe07c6d167fcc255a59b062e9a7c7968423acad
smret <smreteab1@gmail.com>
2025-10-21 17:25:44 +0200

    lokal change

--- src/script/pcNameInfo.js
+++ src/script/pcNameInfo.js
@@ -6,40 +6,18 @@ async function initSystemInfo() {
 }
 
 async function updateSystemInfo(){
-    //let futureText = "";
-   // const sysInfoDiv = document.getElementById("pcInfoDisplay");
-    //
-
-   /* futureText = "PC Name: " + await window.systemInfo.getGlobal('pcName');
-    futureText += "\nIP: " + await window.systemInfo.getGlobal('currentIP');
-    futureText += "\nOS: " + await window.systemInfo.getGlobal('osVersion');
-    futureText += "\nModel: " + await window.systemInfo.getGlobal('pcModel');
-    futureText += "\nUser: " + await window.systemInfo.getGlobal('userName');
-
-   */
-
-   /* const updatesAvailable = await window.systemInfo.getGlobal('updatesAvailable');
-
-    if(updatesAvailable === null){
-        futureText += "\nUpdates: Initializing...";
-    } else if(updatesAvailable){
-        futureText += "\nUpdates: Available";
-    } else if(!updatesAvailable){
-        futureText += "\nUpdates: None";
-    } else futureText += "\nUpdates: Unknown";*/
+    
     getPcValues();
 
-
-    //sysInfoDiv.innerText = futureText;
 }
-//
+// Sets all values in to respectiv place when it is called by other functions.
 async function getPcValues() {
     document.getElementById("ipAddressLabel").textContent = "IP-Adress: " + await window.systemInfo.getGlobal("currentIP");
     document.getElementById("UserNameLabel").textContent = "Användarenamn: " + await window.systemInfo.getGlobal("userName");
     document.getElementById("pcNameLabel").textContent = "Dator Namn: " + await window.systemInfo.getGlobal("pcName");
     document.getElementById("pcModelLabel").textContent = "Dator Modell: " + await window.systemInfo.getGlobal("pcModel");
     document.getElementById("osVersionLabel").textContent = "OS Version: " + await window.systemInfo.getGlobal("osVersion"); 
-    document.getElementById("lastRebootLabel").textContent= "Senaste Omstart: " + await window.systemInfo.getGlobal("compOnTimeHours");
+    document.getElementById("lastRebootLabel").textContent= "Senaste Omstart: " + await window.systemInfo.getGlobal("compOnTimeHours"); 
     //document.getElementById("phoneSpan").innerHTML = await window.systemInfo.getGlobal();
     //document.getElementById("linkSpan").innerHTML = await window.systemInfo.getGlobal();
 
@@ -54,5 +32,11 @@ async function getPcValues() {
     } else if(!updatesAvailable){
         isUpdateAvl.textContent = "Windows-uppdatering tillgänglig: Nej";
     } else isUpdateAvl.textContent = "Upddatering: Ett fel uppstod";
+    //
+    const contentDiv = document.getElementById("teleLink");
+    const adminDoc = window.dbManager.getAdminDocument();
 
+    if (contentDiv && adminDoc) {
+        contentDiv.textContent = "Kontakt oss: " + adminDoc.suppNr;
+    }
 }
